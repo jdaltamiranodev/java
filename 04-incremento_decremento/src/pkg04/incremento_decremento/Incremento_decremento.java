3ee74572dc2680d0575b407b9733efb911cb435c
David Altamirano <jdaltamiranodev@gmail.com>
2024-01-23 21:27:32 +0100

    Se realizan ejemplos de operadores de incremento y decremento y la diferencia que tiene su uso como sufijo y prefijo en java.

---
+++ 04-incremento_decremento/src/pkg04/incremento_decremento/Incremento_decremento.java
@@ -0,0 +1,22 @@
+// incremento decremento
+package pkg04.incremento_decremento;
+
+public class Incremento_decremento {
+
+    public static void main(String[] args) {
+        int x = 5, y;
+        
+        /* En el primer caso se asigna y luego incrementa porque se pone ++
+           como sufijo
+        ***En el segundo caso se incrementa x y luego se asigna a y. En
+           este caso ++ se pone como prefijo */
+        y = x++; // Se asigna el valor de x a y y despues se incrementa x en 1
+        System.out.println(y); // 5
+        System.out.println(x); // 6
+        y = ++x; // Se incrementa x en 1 y despues se asigna a y
+        
+        System.out.println(y); // 6
+        System.out.println(x); // 6
+    }
+    
+}
