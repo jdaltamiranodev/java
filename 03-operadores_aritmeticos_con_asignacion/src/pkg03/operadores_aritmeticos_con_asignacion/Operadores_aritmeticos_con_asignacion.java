83bec617c29069a84b1556ec7b684e4e858d00af
David Altamirano <jdaltamiranodev@gmail.com>
2024-01-23 21:26:44 +0100

    Se trabajan ejemplos de operadores aritmeticos con el operador de asignación para presentar una forma abreviada de utilizarlos en java.

---
+++ 03-operadores_aritmeticos_con_asignacion/src/pkg03/operadores_aritmeticos_con_asignacion/Operadores_aritmeticos_con_asignacion.java
@@ -0,0 +1,17 @@
+
+package pkg03.operadores_aritmeticos_con_asignacion;
+
+public class Operadores_aritmeticos_con_asignacion {
+
+    public static void main(String[] args) {
+        int numero = 10;
+        int numero1 = 10;
+        
+        numero += 5; // numero = numero + 5;
+        numero -= 5; // numero = numero - 5;
+        
+        System.out.println("" + numero);
+        System.out.println("" + numero1);
+    }
+    
+}
