f9a38936a976f6a75aee92d3258320d8116118ce
David Altamirano <jdaltamiranodev@gmail.com>
2024-02-14 21:32:50 +0100

    Se añade ejercicio de bucles for, while, do while en Java.

---
+++ Ejercicios_Practica/03-bucles/Ejercicio_9/src/ejercicio_9/Ejercicio_9.java
@@ -0,0 +1,15 @@
+/*
+    Ejercicio 9: Escribir todos los números del 100 al 0 de 7 en 7
+*/
+package ejercicio_9;
+
+public class Ejercicio_9 {
+
+    public static void main(String[] args) {
+        
+        for (int i = 100; i >= 0; i -= 7) {
+            System.out.println(i);
+        }
+    }
+    
+}
