2a173eb2c7bcebb6b251654db32e1ee9742ea68e
David Altamirano <jdaltamiranodev@gmail.com>
2024-03-05 22:38:25 +0100

    Se realiza ejercicio del bloque de matrices en Java

---
+++ Ejercicios_Practica/05-matrices/Ejercicio_4/src/ejercicio_4/Ejercicio_4.java
@@ -0,0 +1,32 @@
+/*
+    Ejercicio 4: Crear una matriz de tamaño 7x7 y rellenarla de forma que los
+    elementos de la diagonal principal sean 1 y el resto 0
+*/
+package ejercicio_4;
+
+public class Ejercicio_4 {
+
+    public static void main(String[] args) {
+        int matriz[][] = new int[7][7];
+        for (int[] matriz1 : matriz) {
+            for (int j = 0; j < matriz.length; j++) {
+                matriz1[j] = 0;
+            }
+        }
+        
+        // Llenar la diagonal principal de 1
+        for (int posicion = 0; posicion < matriz.length; posicion++) {
+            matriz[posicion][posicion] = 1;
+        }
+        
+        // Mostrar la matriz
+        //for (int[] matriz1 : matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
