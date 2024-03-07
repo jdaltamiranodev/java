285839e169dbd8a48e315ef4bea98c4c9a9637ef
David Altamirano <jdaltamiranodev@gmail.com>
2024-03-07 20:10:52 +0100

    Se realiza una introducción a los constructores en Java

---
+++ 18_4-ProgramacionOrientadaAObjetos/src/metodoConstructor/Main.java
@@ -0,0 +1,10 @@
+
+package metodoConstructor;
+
+public class Main {
+    public static void main(String [] args){
+        Persona persona = new Persona("Juan", 3);
+        
+        persona.mostrarDatos();
+    }
+}
---
+++ 18_4-ProgramacionOrientadaAObjetos/src/metodoConstructor/Persona.java
@@ -0,0 +1,26 @@
+
+package metodoConstructor;
+
+public class Persona {
+    // Atributos
+    String nombre;
+    int edad;
+    
+    // Métodos
+    
+    // Método Constructor
+    /*public Persona(String _nombre, int _edad){
+        nombre = _nombre;
+        edad = _edad;
+    }*/
+    
+    public Persona(String nombre, int edad){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    
+    public void mostrarDatos(){
+        System.out.println("El nombre es: " + nombre);
+        System.out.println("La edad es: " + edad);
+    }
+}
